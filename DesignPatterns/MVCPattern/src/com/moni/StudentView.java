74fd9d42ba14dd7b774f894c3ae024076033349e
Monica Shopova <monika.shopova@gmail.com>
2015-05-07 12:22:59 +0300

    some

---
+++ DesignPatterns/MVCPattern/src/com/moni/StudentView.java
@@ -0,0 +1,13 @@
+package com.moni;
+
+/**
+ * Created by Monica Shopova
+ * monika.shopova@gmail.com
+ */
+public class StudentView {
+
+    public void printStudents(String studentName, String studentRollNo) {
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student rollNo: " + studentRollNo);
+    }
+}
